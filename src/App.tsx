27cc5967b5939eca9fe418137546f7f1cd1e63f8
vermaanurag1532 <vermaanurag1532@gmail.com>
2023-08-17 20:08:19 +0530

    style: add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Header from "./Header/header";
 import AllClothes from "./AllClothes/AllClothes";
 import "./App.css";
 import ChoiceBased from "./ChoiceBased/ChoiceBased";
 import PromptBased from "./PromptBased/PromptBased";
 import DataContext from "./context/dataContext";
+import Footer from "./Footer/footer";
 
 function App() {
   const [searchType, setSearchType] = useState("CHOICE");
@@ -14,32 +16,37 @@ function App() {
   });
 
   return (
+    <div>
+      <Header />
 
-    <DataContext.Provider value={{ data, setData }}>
-      <div className="buttons">
-        <button
-          className="chooseButton"
-          type="button"
-          onClick={() => {
-            setSearchType("CHOICE");
-          }}
-        >
-          Choose
-        </button>
-        <button
-          className="promptButton"
-          type="button"
-          onClick={() => {
-            setSearchType("PROMPT");
-          }}
-        >
-          Prompt
-        </button>
-      </div>
-      {searchType === "CHOICE" && <ChoiceBased />}
-      {searchType === "PROMPT" && <PromptBased />}
-      {data.isSearched && <AllClothes />}
-    </DataContext.Provider>
+      <DataContext.Provider value={{ data, setData }}>
+        <div className="buttons">
+          <button
+            className="chooseButton"
+            type="button"
+            onClick={() => {
+              setSearchType("CHOICE");
+            }}
+          >
+            Choose
+          </button>
+          <button
+            className="promptButton"
+            type="button"
+            onClick={() => {
+              setSearchType("PROMPT");
+            }}
+          >
+            Prompt
+          </button>
+        </div>
+        {searchType === "CHOICE" && <ChoiceBased />}
+        {searchType === "PROMPT" && <PromptBased />}
+        {data.isSearched && <AllClothes />}
+      </DataContext.Provider>
+
+      <Footer />
+    </div>
   );
 }
 
