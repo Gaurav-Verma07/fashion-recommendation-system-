c00b45cc04734050ae017ec5358318e2bf4af214
vermaanurag1532 <vermaanurag1532@gmail.com>
2023-08-17 15:21:37 +0530

    style: add css

--- src/PromptBased/PromptBased.tsx
+++ src/PromptBased/PromptBased.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useContext } from "react";
 import DataContext from "../context/dataContext";
-import classes from "./promptBased.module.css"
+import classes from "./promptBased.module.css";
 
 const PromptBased = () => {
   const [prompt, setPrompt] = useState<string>("");
@@ -19,20 +19,40 @@ const PromptBased = () => {
 
   return (
     <section>
-      <div>
-        <textarea className={classes.textBox}
-          placeholder="Write spme prompt for Suggestions ..."
-          onChange={(e: any) => {
-            setPrompt(e.target.value);
-          }}
-          name=""
-          id=""
-          cols={30}
-          rows={10}
-        ></textarea>
-        <button className={classes.searchButton} type="button" onClick={handleSearch}>
-          Search
-        </button>
+      <div className={classes.container}>
+        <div className={classes.prompt}>
+          <div className={classes.promptHeading}>
+            <h3>Describe your fashion</h3>
+            <h4>
+              What do you want to wear, you can use a single word or complete
+              sentence
+            </h4>
+          </div>
+          <textarea
+            className={classes.textBox}
+            placeholder="Write spme prompt for Suggestions ..."
+            onChange={(e: any) => {
+              setPrompt(e.target.value);
+            }}
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button
+            className={classes.searchButton}
+            type="button"
+            onClick={handleSearch}
+          >
+            Generate
+          </button>
+        </div>
+        <div className={classes.generatedImage}>
+          <div className={classes.promptHeading}>
+            <h3>Your Desired GenAI Image</h3>
+          </div>
+          <div className={classes.genAiImage}></div>
+        </div>
       </div>
     </section>
   );
