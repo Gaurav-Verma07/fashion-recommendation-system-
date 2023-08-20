abcb703c169d594d7e00e52f5dd964ed0c8ffaa8
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-08-20 17:46:07 +0530

    feat: features section

--- src/components/Herobox/Herobox.tsx
+++ src/components/Herobox/Herobox.tsx
@@ -1,10 +1,6 @@
 import { Button, Text, Title } from "@mantine/core";
 import classes from "./Herobox.module.css";
 import { useNavigate } from "react-router-dom";
-import logo from "../../assets/uc1.png";
-import logo1 from "../../assets/uc3.png";
-import logo2 from "../../assets/uc2.png";
-import logo3 from "../../assets/uc4.png";
 
 const Herobox = () => {
   const navigate = useNavigate();
@@ -37,57 +33,6 @@ const Herobox = () => {
           </Button>
         </div>
       </section>
-      <div className={classes.container}>
-        <div className={classes.uc1}>
-          <div className={classes.uc1Image}>
-            <img src={logo} alt="" />
-          </div>
-          <div className={classes.uc1Content}>
-            <h1>Sales Order Creation</h1>
-            <p>
-              Offer personalized outfit suggestions using past purchases,
-              browsing history, and style preferences to improve shopping and
-              boost successful buys.
-            </p>
-          </div>
-        </div>
-        <div className={classes.uc1}>
-          <div className={classes.uc2Content}>
-            <h1>Learning and Recommendation</h1>
-            <p>
-              Provide trend insights via social media analysis, keeping users
-              stylish and informed about current fashion.
-            </p>
-          </div>
-          <div className={classes.uc2Image}>
-            <img src={logo1} alt="" />
-          </div>
-        </div>
-        <div className={classes.uc1}>
-          <div className={classes.uc3Image}>
-            <img src={logo2} alt="" />
-          </div>
-          <div className={classes.uc1Content}>
-            <h1>Virtual Stylist</h1>
-            <p>
-              Act as a virtual stylist, offering personalized outfit advice for
-              different occasions, boosting users' fashion confidence.
-            </p>
-          </div>
-        </div>
-        <div className={classes.uc1}>
-          <div className={classes.uc2Content}>
-            <h1>Chat Bot Integration</h1>
-            <p>
-              Allow users to upload wardrobe photos; AI suggests outfits using
-              current items and new buys
-            </p>
-          </div>
-          <div className={classes.uc4Image}>
-            <img src={logo3} alt="" />
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import Features from "../../components/Features/Features";
 import Herobox from "../../components/Herobox/Herobox";
 
 const HomePage = () => {
   return (
     <div>
       <Herobox />
+      <Features />
     </div>
   );
 };
