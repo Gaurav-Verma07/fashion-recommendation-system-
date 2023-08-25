f02a946f1631e80e20a15f67d80ff0a6d78f16fd
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-08-25 15:56:30 +0530

    feat: nodemon

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-app.use("/", () => {
+app.use("/", (req, res, next) => {
   console.log("route working");
+  res.json({
+    name: "server",
+  });
 });
 
-module.exports= app;
+module.exports = app;
