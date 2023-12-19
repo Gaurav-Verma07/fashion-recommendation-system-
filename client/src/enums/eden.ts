c988835e7f4bc42b6dd0aa3fdf710b9170cacb8a
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-12-19 19:36:45 +0530

    feat:api

--- client/src/enums/eden.ts
+++ client/src/enums/eden.ts
@@ -1,6 +1,6 @@
 export enum EDENAI{
     PROVIDERS= 'openai',
-    NUM_IMAGES= 2,
+    NUM_IMAGES= 4,
     RESOLUTION= '256x256',
     
 }
--- client/src/utils/edenAIApi.ts
+++ client/src/utils/edenAIApi.ts
@@ -4,7 +4,7 @@ export const edenAIApi = async (prompt: string) => {
   const url = "https://api.edenai.run/v2/image/generation";
 
   const headers = {
-    authorization: `Bearer YOUR_API_KEY`,
+    authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMWE4MDU4NzEtNWNlOC00NGU0LTgxNzEtMWRhM2NmZGIyNmM3IiwidHlwZSI6ImFwaV90b2tlbiJ9.Pdaf08Or7PG2N10aji2-mv2Qa7c99bfAhqRyDnKoEwU`,
     "Content-Type": "application/json",
   };
   const data = {
