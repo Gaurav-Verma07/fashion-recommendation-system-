7b21a6797d697279d9f472e2b4ebcf60022e0932
Gaurav Verrma <63046396+Gaurav-Verma07@users.noreply.github.com>
2023-12-22 11:35:25 +0530

    Update footer.tsx

--- client/src/components/Footer/footer.tsx
+++ client/src/components/Footer/footer.tsx
@@ -3,7 +3,10 @@ import classes from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={classes.footer}>
-      <center className={classes.footerHeading}>Team : While</center>
+      <center className={classes.footerHeading}>Design by:Anshu verma </center>
+      <center className={classes.footerHeading}>          Anshu verma </center>
+      <center className={classes.footerHeading}>Design by:Anshu verma </center>
+      <center className={classes.footerHeading}>Design by:Anshu verma </center>
     </div>
   );
 };
